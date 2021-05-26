72902a647d1d0d69dfa7787b16240bb134b03f57
Bhupati Patel <bhupati.patel@oracle.com>
2021-05-26 13:28:02 +0530

    2nd Commit

--- src/com/patel/bhupati/HelloWorld.java
+++ src/com/patel/bhupati/HelloWorld.java
@@ -2,6 +2,6 @@ package com.patel.bhupati;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World 2");
     }
 }
